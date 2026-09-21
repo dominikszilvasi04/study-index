b2c880670f492666e7834917f6b9caa361863f2d
Dominik Szilvasi <dominik.szilvasi.2024@mumail.ie>
2026-09-21 18:34:19 +0100

    set up main window set up outside of app.py for decoupling purposes

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import sys
+from PySide6.QtWidgets import QApplication
 
-from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+def main():
+    application = QApplication(sys.argv)
+    application.setApplicationName("StudyIndex")
+    return application.exec()
 
-application = QApplication(sys.argv)
-application.setApplicationName("StudyIndex")
-
-window = QMainWindow()
-window.setWindowTitle("StudyIndex")
-window.setCentralWidget(QLabel("No modules yet"))
-window.show()
-
-raise SystemExit(application.exec())
+if __name__ == "__main__":
+    raise SystemExit(main())
